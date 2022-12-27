8be9893bf0eb16c319517e37f439002caa25c693
ayalaavital <49597407+ayalaavital@users.noreply.github.com>
2022-12-27 13:53:29 +0200

    warm-up task

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,9 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+    if (_chatBot != nullptr){
+        delete _chatBot;
+    }
 
     ////
     //// EOF STUDENT CODE
